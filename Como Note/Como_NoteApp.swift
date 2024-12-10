6add0dc9635fcdc7598d5549d4f2bddbd33308cc
Usama Azam <usamaazam10@gmail.com>
2024-12-10 15:46:42 +0500

    Initial Commit

---
+++ Como Note/Como_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Como_NoteApp.swift
+//  Como Note
+//
+//  Created by Usama Azam on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Como_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Como Note/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Como Note
+//
+//  Created by Usama Azam on 10/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
